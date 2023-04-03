adf3c997d36bb803e181becc04df13e659eed3b8
Nastya Yermakova <ermkvnst@icloud.com>
2023-04-03 22:19:40 +0300

    Додає вирішення задач

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+      </div>`;
+    })
+    .join("");
+}
+
+const gallery = document.querySelector(".gallery");
+const items = createGalleryItem(galleryItems);
+gallery.insertAdjacentHTML("beforeend", items);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  // captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
